2c3f0d14682b7cc0413cea59840c571f9bf3d2fc
Ray Lee <rlee@thefind.com>
2015-02-18 21:26:15 -0800

    Added onClick events

--- app/src/main/java/com/swpbiz/myinstagram/MainActivity.java
+++ app/src/main/java/com/swpbiz/myinstagram/MainActivity.java
@@ -1,19 +1,35 @@
 package com.swpbiz.myinstagram;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
+    public void handleFacebook(View view) {
+        Toast.makeText(this, "On no.. times up facebook!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleEmail(View view) {
+        Toast.makeText(this, "On no.. times up email!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleAccount(View view) {
+        Toast.makeText(this, "On no.. times up account!", Toast.LENGTH_SHORT).show();
+    }
 
 }
